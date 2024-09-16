69a4533abc6d520aa8eb8676598f0044438ae4d7
chepe <jsozachevez@gmail.com>
2024-09-16 09:47:34 -0600

    creación de archivos necesarios para la caja general

---
+++ app/Http/Controllers/CajaGeneralController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Caja_general;
+use Illuminate\Http\Request;
+
+class CajaGeneralController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Caja_general $caja_general)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Caja_general $caja_general)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Caja_general $caja_general)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Caja_general $caja_general)
+    {
+        //
+    }
+}
