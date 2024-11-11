aefff8d68edf3fa5cfd3a9beeaaa76ed00d5b7cd
Enmanuel Quintero <qenmanuel2@gmail.com>
2024-11-11 10:32:20 -0600

    upgrade en visual de  arqueo de caja ( sin backend )

--- resources/views/layouts/arqueo.blade.php
+++ resources/views/layouts/arqueo.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arqueo</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
+    <link rel="stylesheet" href="https://storage.googleapis.com/non-spec-apps/mio-icons/latest/outline.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
+
+
 </head>
 <body>
     @include('components.sidebar')
